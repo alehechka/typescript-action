ec79e8f1e03e485d3d48265b104c903cb0dc0c22
Adam Lehechka <ajlehechka@gmail.com>
2022-02-24 11:20:31 -0600

    build package

--- dist/index.js
+++ dist/index.js
@@ -41,6 +41,10 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const ms = core.getInput('milliseconds');
+            const flag = core.getBooleanInput('flag');
+            const other = core.getBooleanInput('other');
+            flag === true ? core.info('flag is true') : core.info('flag is false');
+            other === true ? core.info('flag is true') : core.info('flag is false');
             core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
             core.debug(new Date().toTimeString());
             yield (0, wait_1.wait)(parseInt(ms, 10));
